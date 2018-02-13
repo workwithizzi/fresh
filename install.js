7396b808e69a6a3619eb3893ac8428eaa90afc69
Yisrael Grimes <yisrael@tubemedia.co>
2018-02-13 00:22:59 -0600

    updated install script

--- install.js
+++ install.js
@@ -18,69 +18,27 @@ var node = {
 
 // Setup Gulpy
 
-// 1. Copy gulpfile.js
-// Goal: If there isn't a gulpfile.js in the project root,
-// then copy the one from node_modules/gulpy.
-fs.copy( node.gulpyD + '/gulpfile.js', './gulpfile.js', {
+// Copy gulpfile.js
+fs.copy(node.gulpyD + '/gulpfile.js', './gulpfile.js', {
 	overwrite: false,
 	preserveTimestamps: true
-	}, err => {
+}, err => {
 	if (err) return console.error(err)
 })
-
-
-// 2. Copy gulp.options.js
-// Goal: If there isn't a gulp.options.js in the project root,
-// then copy the one from node_modules/gulpy.
-fs.copy( node.gulpyD + '/gulp.options.js', './gulp.options.js', {
-	overwrite: false,
-	preserveTimestamps: true
-	}, err => {
-	if (err) return console.error(err)
-})
-
-
-// 3. Symlink to Gulpy tasks
-// Goal: If there's not already a gulpy directory in the root,
-// create a symlink to the gulp tasks directory ('node_modules/gulpy/gulp')
-// Would it be better to just copy this to the root or symlink?? I was thinking it would be better to symlink since we don't want to edit the gulp tasks from within this project. Another idea was to symlink it, then if you need to mdoify a task for this particular project you could run a command to turn remove the symlink and copy the actual files so they could be tracked.
-
-fs.ensureSymlink(node.gulpyD + '/gulp', './gulp', err => {
-	if (err) return console.error(err)
-})
-
-// Temporary--Copying instead of Symlinking
-// fs.copy( node.gulpyD + '/gulp', './gulp', {
-// 	overwrite: false,
-// 	preserveTimestamps: true
-// 	}, err => {
-// 	if (err) return console.error(err)
-// })
-
 
 // ----------------
 // Setup Project Scaffold
 
-// 4. Dev files
-// Goal: Copy directory from 'node_modules/fresh-scaffold/dev' to the root.
-// Skip task if './dev' already exists.
-fs.copy( node.scaffoldD + '/dev', './dev', {
+// Dev files
+fs.copy(node.scaffoldD + '/src', './src', {
 	overwrite: false,
 	preserveTimestamps: true
-	}, err => {
+}, err => {
 	if (err) return console.error(err)
 })
 
 
-// 5. Rename the cloned readme
-// Goal: Rename the included readme file so that it stays in the repo for
-// reference. Need to set this up so that it only runs this task on the
-// initial install. I guess the best way to do that would be to skip the
-// task if there's already a 'README_fresh.md' in the root?
-//
-// This task and #6 throws an error if you run `yarn install` again after
-// the initial install.
-
+// Rename the cloned readme
 // fs.move('./README.md', './README_fresh.md', {
 // 	overwrite: false
 // 	}, err => {
@@ -89,44 +47,37 @@ fs.copy( node.scaffoldD + '/dev', './dev', {
 
 // HACK: Just to make it work without errors.
 fs.stat('./README_fresh.md', function(err, stat) {
-	if(err == null) {
+	if (err == null) {
 		// If file exists: do nothing
 	} else {
 		// If file doesn't exist
 		fs.renameSync('./README.md', './README_fresh.md')
-		fs.copy( node.scaffoldD + '/rootfiles/README.md', './README.md', {
+		fs.copy(node.scaffoldD + '/rootfiles/README.md', './README.md', {
 			overwrite: false,
 			preserveTimestamps: true
-			}, err => {
-				if (err) return console.error(err)
+		}, err => {
+			if (err) return console.error(err)
 		})
 	}
 });
 
 
-
-// 6. Copy dev files templates into the project
-// Goal: Copy all the files from 'node_modules/fresh-scaffold/rootfiles' to the
-// project root. Need to set it up so that it ignores the README.md file if
-// the rename readme task isn't run (see #5)
-fs.copy( node.scaffoldD + '/rootfiles', './', {
+// Copy dev files templates into the project
+fs.copy(node.scaffoldD + '/rootfiles', './', {
 	overwrite: false,
 	preserveTimestamps: true
-	}, err => {
-		if (err) return console.error(err)
+}, err => {
+	if (err) return console.error(err)
 })
 
 
 // ----------------
 // Setup Luscious
 
-// 7. Setup Luscious-sass
-// Goal: Create a symlink from './node_modules/luscious-sass' to './dev/styles/01_utils/luscious'.
-// Wondering the same thing about this one that I was for #3 with the Gulp tasks.
-
-fs.ensureSymlink(node.lusciousD, './dev/styles/utils/luscious', err => {
-	if (err) return console.error(err)
-})
+// Setup Luscious-sass
+// fs.ensureSymlink(node.lusciousD, './dev/styles/utils/luscious', err => {
+// 	if (err) return console.error(err)
+// })
 
 // Temporary--Copying instead of Symlinking
 // fs.copy( node.lusciousD, './dev/styles/01_utils/luscious', {
