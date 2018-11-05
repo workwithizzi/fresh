d38a309df79127983024e1eb0a6c3c86f93729f9
IvaTsu <ivan.tsud@gmail.com>
2018-11-05 19:27:57 +0200

    Add 'no-tabs' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,7 @@
 module.exports = {
-	parser: "babel-eslint",
-	extends: ["plugin:prettier/recommended"]
+	parser: 'babel-eslint',
+	extends: ['plugin:prettier/recommended'],
+	rules: {
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+	},
 };
