d3c64e028ba0731dbb3341b1ec22321ba1202674
IvaTsu <ivan.tsud@gmail.com>
2018-11-05 20:29:59 +0200

    Check scripts:beautify task

--- src/js/nav.js
+++ src/js/nav.js
@@ -5,34 +5,34 @@
 // ------------------------------------------------------------------
 
 // Set button to click.
-var navBtn = document.getElementById("js-toggle-menu");
+var navBtn = document.getElementById('js-toggle-menu');
 
 // Click the button.
 
 // Control svg animation of button.
 navBtn.onclick = function() {
 	// Toggle class "opened". Set also aria-expanded to true or false.
-	if (-1 !== navBtn.className.indexOf("is-active")) {
-		navBtn.className = navBtn.className.replace(" is-active", "");
-		navBtn.setAttribute("aria-expanded", "false");
+	if (-1 !== navBtn.className.indexOf('is-active')) {
+		navBtn.className = navBtn.className.replace(' is-active', '');
+		navBtn.setAttribute('aria-expanded', 'false');
 	} else {
-		navBtn.className += " is-active";
-		navBtn.setAttribute("aria-expanded", "true");
+		navBtn.className += ' is-active';
+		navBtn.setAttribute('aria-expanded', 'true');
 	}
 };
 
 // Add 'is-active' class to '.c-nav'
 $(function() {
-	$("#js-toggle-menu").click(function(e) {
+	$('#js-toggle-menu').click(function(e) {
 		e.preventDefault();
-		$(".js-nav").toggleClass("is-active");
+		$('.js-nav').toggleClass('is-active');
 	});
 });
 
 // Add 'sidebar-active' class to '<main>'
 $(function() {
-	$("#js-toggle-menu").click(function(e) {
+	$('#js-toggle-menu').click(function(e) {
 		e.preventDefault();
-		$("main").toggleClass("sidebar-active");
+		$('main').toggleClass('sidebar-active');
 	});
 });
