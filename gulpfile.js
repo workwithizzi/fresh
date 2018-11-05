b140416a80ea820578ccf17f57b740a3590f719d
IvaTsu <ivan.tsud@gmail.com>
2018-11-05 19:28:22 +0200

    ESlint autoformat

--- gulpfile.js
+++ gulpfile.js
@@ -5,58 +5,58 @@
 var openBrowserWindow = false;
 
 var base = {
-	src: "./src",
-	build: "./build"
-},
-	srcViews = base.src + "/views",
-	srcStyles = base.src + "/sass",
-	srcScripts = base.src + "/js",
-	srcFonts = base.src + "/fonts",
-	srcImages = base.src + "/images",
-	buildCss = base.build + "/css",
-	buildJs = base.build + "/js",
-	buildImages = base.build + "/images",
-	buildFonts = base.build + "/fonts",
+		src: './src',
+		build: './build',
+	},
+	srcViews = base.src + '/views',
+	srcStyles = base.src + '/sass',
+	srcScripts = base.src + '/js',
+	srcFonts = base.src + '/fonts',
+	srcImages = base.src + '/images',
+	buildCss = base.build + '/css',
+	buildJs = base.build + '/js',
+	buildImages = base.build + '/images',
+	buildFonts = base.build + '/fonts',
 	// ------------------------------------
 	// Styles
 	// ------------------------------------
 	styles = {
 		// Paths
-		input: srcStyles + "/**/*.s+(a|c)ss",
+		input: srcStyles + '/**/*.s+(a|c)ss',
 		output: buildCss,
 
 		// Config Options
 		opts: {
 			output: {
 				errLogToConsole: true,
-				outputStyle: "expanded",
-				sourceComments: "true",
-				indentType: "tab",
-				indentWidth: "1"
+				outputStyle: 'expanded',
+				sourceComments: 'true',
+				indentType: 'tab',
+				indentWidth: '1',
 			},
-			prefixer: ["last 2 versions", "ie >= 9", "and_chr >= 2.3"],
+			prefixer: ['last 2 versions', 'ie >= 9', 'and_chr >= 2.3'],
 			lint: false,
 			lintConfig: {
-				configFile: "./.sasslint.yml"
+				configFile: './.sasslint.yml',
 			},
 			lintFixConfig: {
-				configFile: "./.sasslint-fix.yml"
+				configFile: './.sasslint-fix.yml',
 			},
-			quoteStyle: "single"
-		}
+			quoteStyle: 'single',
+		},
 	},
 	// ------------------------------------
 	// Scripts
 	// ------------------------------------
 	scripts = {
 		// Paths
-		input: srcScripts + "/**/*.js",
+		input: srcScripts + '/**/*.js',
 		output: buildJs,
 		beautifyOutput: srcScripts,
 
 		jquery: {
-			input: "./node_modules/jquery/dist/jquery.min.js",
-			output: buildJs
+			input: './node_modules/jquery/dist/jquery.min.js',
+			output: buildJs,
 		},
 
 		// Config Options
@@ -65,31 +65,31 @@ var base = {
 			beautify: {
 				// Options: git.io/vVyYB
 				indent_size: 2,
-				indent_with_tabs: true
-			}
-		}
+				indent_with_tabs: true,
+			},
+		},
 	},
 	// ------------------------------------
 	// HTML/Views/Database
 	// ------------------------------------
 	html = {
 		// Paths
-		srcFiles: srcViews + "/**/*.html",
-		productionFiles: base.build + "/**/*.html",
-		converted: srcViews + "/converted-html",
+		srcFiles: srcViews + '/**/*.html',
+		productionFiles: base.build + '/**/*.html',
+		converted: srcViews + '/converted-html',
 
 		// Config Options
 		opts: {
 			collapseWhitespace: true,
 			removeComments: true,
 			minifyCSS: true,
-			minifyJS: true
-		}
+			minifyJS: true,
+		},
 	},
 	views = {
 		// Paths
-		input: srcViews + "/**/*.pug",
-		partials: srcViews + "/**/_*.pug",
+		input: srcViews + '/**/*.pug',
+		partials: srcViews + '/**/_*.pug',
 		output: base.build,
 
 		// Config Options
@@ -97,49 +97,49 @@ var base = {
 			useData: true,
 			lint: true,
 			output: {
-				pretty: "	",
-				basedir: srcViews
+				pretty: '	',
+				basedir: srcViews,
 			},
 			prettyPug: {
 				omit_empty_lines: true,
 				fill_tab: true,
-				omit_div: true
+				omit_div: true,
 				// tab_size: 2   // Only used with 'fill_tab' == false
-			}
-		}
+			},
+		},
 	},
 	db = {
-		input: srcViews + "/data/**/*.json",
+		input: srcViews + '/data/**/*.json',
 		output: srcViews,
-		fileName: "data.json",
-		fileDir: srcViews
+		fileName: 'data.json',
+		fileDir: srcViews,
 	},
 	// ------------------------------------
 	// Fonts/Images/Misc Dependencies
 	// ------------------------------------
 	fonts = {
-		input: srcFonts + "/**/*",
+		input: srcFonts + '/**/*',
 		output: buildFonts,
 		dependency: {
-			input: "./node_modules/open-sans-fonts/open-sans/**/*",
-			output: srcFonts + "/open-sans/"
-		}
+			input: './node_modules/open-sans-fonts/open-sans/**/*',
+			output: srcFonts + '/open-sans/',
+		},
 	},
 	images = {
 		// Paths
-		input: base.src + "/images/**/*.+(png|jpg|jpeg|gif|svg|ico)",
-		output: base.build + "/images/",
+		input: base.src + '/images/**/*.+(png|jpg|jpeg|gif|svg|ico)',
+		output: base.build + '/images/',
 
 		// Config Options
 		opts: {
-			interlaced: true
-		}
+			interlaced: true,
+		},
 	},
 	owlCarousel = {
-		inputCss: "./node_modules/owl.carousel/dist/assets/owl.carousel.css",
-		outputCss: buildCss + "/",
-		inputJs: "./node_modules/owl.carousel/dist/owl.carousel.js",
-		outputJs: buildJs + "/"
+		inputCss: './node_modules/owl.carousel/dist/assets/owl.carousel.css',
+		outputCss: buildCss + '/',
+		inputJs: './node_modules/owl.carousel/dist/owl.carousel.js',
+		outputJs: buildJs + '/',
 	};
 
 // ------------------------------------
@@ -155,7 +155,7 @@ var config = {
 		injectChanges: true,
 		notify: false,
 		port: 3000,
-		ui: { port: 8080 }
+		ui: { port: 8080 },
 	},
 	// Comment out anything in the array that you
 	// don't want to watch/trigger build on change.
@@ -164,48 +164,48 @@ var config = {
 		views: views.input,
 		styles: styles.input,
 		scripts: scripts.input,
-		reload: html.productionFiles
-	}
+		reload: html.productionFiles,
+	},
 };
 
 // ------------------------------------------------------------------
 // Plugins
 // ------------------------------------------------------------------
-var g = require("gulp"),
-	sass = require("gulp-sass"),
-	sassLint = require("gulp-sass-lint"),
-	prefix = require("gulp-autoprefixer"),
-	cssnano = require("gulp-cssnano"),
-	uglify = require("gulp-uglify"),
-	beautify = require("gulp-beautify"),
-	sourcemaps = require("gulp-sourcemaps"),
-	htmlmin = require("gulp-htmlmin"),
-	html2pug = require("gulp-html2pug"),
-	prettyPug = require("gulp-pug-beautify"),
-	puglint = require("gulp-pug-lint"),
-	pug = require("gulp-pug"),
-	data = require("gulp-data"),
-	path = require("path"),
-	merge = require("gulp-merge-json"),
-	imagemin = require("gulp-imagemin"),
-	cache = require("gulp-cache"),
-	fs = require("fs-extra"),
-	useref = require("gulp-useref"),
-	gulpif = require("gulp-if"),
-	browserSync = require("browser-sync"),
-	runSequence = require("run-sequence"),
-	replaceQuotes = require("gulp-replace-quotes"),
-	rename = require("gulp-rename"),
-	del = require("del"),
-	usage = require("gulp-help-doc"),
-	plumber = require("gulp-plumber"),
-	prettierEslint = require("gulp-prettier-eslint"),
-	run = require("gulp-run"),
+var g = require('gulp'),
+	sass = require('gulp-sass'),
+	sassLint = require('gulp-sass-lint'),
+	prefix = require('gulp-autoprefixer'),
+	cssnano = require('gulp-cssnano'),
+	uglify = require('gulp-uglify'),
+	beautify = require('gulp-beautify'),
+	sourcemaps = require('gulp-sourcemaps'),
+	htmlmin = require('gulp-htmlmin'),
+	html2pug = require('gulp-html2pug'),
+	prettyPug = require('gulp-pug-beautify'),
+	puglint = require('gulp-pug-lint'),
+	pug = require('gulp-pug'),
+	data = require('gulp-data'),
+	path = require('path'),
+	merge = require('gulp-merge-json'),
+	imagemin = require('gulp-imagemin'),
+	cache = require('gulp-cache'),
+	fs = require('fs-extra'),
+	useref = require('gulp-useref'),
+	gulpif = require('gulp-if'),
+	browserSync = require('browser-sync'),
+	runSequence = require('run-sequence'),
+	replaceQuotes = require('gulp-replace-quotes'),
+	rename = require('gulp-rename'),
+	del = require('del'),
+	usage = require('gulp-help-doc'),
+	plumber = require('gulp-plumber'),
+	prettierEslint = require('gulp-prettier-eslint'),
+	run = require('gulp-run'),
 	// Tree
-	archy = require("archy"),
-	map = require("gulp-map"),
-	filetree = require("gulp-filetree"),
-	prettier = require("gulp-plugin-prettier");
+	archy = require('archy'),
+	map = require('gulp-map'),
+	filetree = require('gulp-filetree'),
+	prettier = require('gulp-plugin-prettier');
 
 // ------------------------------------
 // Tasks
@@ -215,8 +215,8 @@ var g = require("gulp"),
  * @task  {default}
  * @group {Main}
  */
-g.task("default", function (callback) {
-	runSequence("compile", ["serve"], callback);
+g.task('default', function(callback) {
+	runSequence('compile', ['serve'], callback);
 });
 
 /**
@@ -224,15 +224,15 @@ g.task("default", function (callback) {
  * @task  {compile}
  * @group {Main}
  */
-g.task("compile", ["styles", "scripts", "jquery", "pug", "images", "fonts"]);
+g.task('compile', ['styles', 'scripts', 'jquery', 'pug', 'images', 'fonts']);
 
 /**
  * Runs tasks: compile, concat, and tree
  * @task  {build}
  * @group {Production}
  */
-g.task("build", function (callback) {
-	runSequence("clean", "compile", "concat", "minify", "tree", callback);
+g.task('build', function(callback) {
+	runSequence('clean', 'compile', 'concat', 'minify', 'tree', callback);
 });
 
 // ------------------------------------
@@ -243,13 +243,13 @@ g.task("build", function (callback) {
  * @task  {styles}
  * @group {Main}
  */
-g.task("styles", () => {
+g.task('styles', () => {
 	return g
 		.src(styles.input)
 		.pipe(gulpif(styles.opts.lint, sassLint(styles.opts.lintConfig)))
 		.pipe(gulpif(styles.opts.lint, sassLint.format()))
 		.pipe(sourcemaps.init())
-		.pipe(sass(styles.opts.output).on("error", sass.logError))
+		.pipe(sass(styles.opts.output).on('error', sass.logError))
 		.pipe(prefix(styles.opts.prefixer))
 		.pipe(sourcemaps.write({ includeContent: false }))
 		.pipe(g.dest(styles.output))
@@ -261,7 +261,7 @@ g.task("styles", () => {
  * @task  {styles:lint}
  * @group {Utilities}
  */
-g.task("styles:lint", () => {
+g.task('styles:lint', () => {
 	return g
 		.src(styles.input)
 		.pipe(sassLint(styles.opts.lintConfig))
@@ -273,21 +273,22 @@ g.task("styles:lint", () => {
  * @task  {styles:fix}
  * @group {Utilities}
  */
-g.task("fix:styles", ["styles:quotes"], () => run("yarn fix").exec());
+g.task('fix:styles', ['styles:quotes'], () => run('yarn fix').exec());
 
 /**
  * Replaces double quotes with single
  * @task  {styles:quotes}
  * @group {Utilities}
  */
-g.task("styles:quotes", () => {
-	g.src(styles.input, { base: "./" })
-		.pipe(replaceQuotes({
-			quote: styles.opts.quoteStyle
-		}))
-		.pipe(g.dest("./"));
+g.task('styles:quotes', () => {
+	g.src(styles.input, { base: './' })
+		.pipe(
+			replaceQuotes({
+				quote: styles.opts.quoteStyle,
+			})
+		)
+		.pipe(g.dest('./'));
 });
-
 
 // ------------------------------------
 // Scripts
@@ -297,7 +298,7 @@ g.task("styles:quotes", () => {
  * @task  {scripts}
  * @group {Main}
  */
-g.task("scripts", () => {
+g.task('scripts', () => {
 	return g
 		.src(scripts.input)
 		.pipe(sourcemaps.init())
@@ -311,7 +312,7 @@ g.task("scripts", () => {
  * @task  {scripts:fix}
  * @group {Utilities}
  */
-g.task("scripts:fix", () => {
+g.task('scripts:fix', () => {
 	return g
 		.src(scripts.input)
 		.pipe(prettierEslint())
@@ -323,7 +324,7 @@ g.task("scripts:fix", () => {
  * @task  {scripts:beautify}
  * @group {Utilities}
  */
-g.task("scripts:beautify", () => {
+g.task('scripts:beautify', () => {
 	return g
 		.src(scripts.input)
 		.pipe(beautify(scripts.opts.beautify))
@@ -336,7 +337,7 @@ g.task("scripts:beautify", () => {
  * @task  {jquery}
  * @group {Utilities}
  */
-g.task("jquery", () => {
+g.task('jquery', () => {
 	return g.src(scripts.jquery.input).pipe(g.dest(scripts.jquery.output));
 });
 
@@ -348,14 +349,14 @@ g.task("jquery", () => {
  * @task  {pug}
  * @group {Main}
  */
-g.task("pug", ["data"], () => {
+g.task('pug', ['data'], () => {
 	return g
-		.src([views.input, "!" + views.partials])
+		.src([views.input, '!' + views.partials])
 		.pipe(
 			gulpif(
 				views.opts.useData,
-				data(function (file) {
-					return JSON.parse(fs.readFileSync(db.fileDir + "/" + db.fileName));
+				data(function(file) {
+					return JSON.parse(fs.readFileSync(db.fileDir + '/' + db.fileName));
 				})
 			)
 		)
@@ -370,7 +371,7 @@ g.task("pug", ["data"], () => {
  * @task  {data}
  * @group {Main}
  */
-g.task("data", () => {
+g.task('data', () => {
 	return g
 		.src(db.input)
 		.pipe(
@@ -379,12 +380,12 @@ g.task("data", () => {
 				edit: (json, file) => {
 					// Extract the filename and strip the extension
 					var filename = path.basename(file.path),
-						primaryKey = filename.replace(path.extname(filename), "");
+						primaryKey = filename.replace(path.extname(filename), '');
 					// Set the filename as the primary key for our JSON data
 					var data = {};
 					data[primaryKey.toUpperCase()] = json;
 					return data;
-				}
+				},
 			})
 		)
 		.pipe(g.dest(db.output));
@@ -395,7 +396,7 @@ g.task("data", () => {
  * @task  {pug:lint}
  * @group {Utilities}
  */
-g.task("pug:lint", () => {
+g.task('pug:lint', () => {
 	return g.src([views.input, views.partials]).pipe(puglint());
 });
 
@@ -404,15 +405,15 @@ g.task("pug:lint", () => {
  * @task  {html2pug}
  * @group {Utilities}
  */
-g.task("html2pug", ["move:html"], () => {
-	return del.sync([html.srcFiles, "!" + html.converted + "/**/*.html"]);
+g.task('html2pug', ['move:html'], () => {
+	return del.sync([html.srcFiles, '!' + html.converted + '/**/*.html']);
 });
 
-g.task("move:html", ["convert:html"], () => {
+g.task('move:html', ['convert:html'], () => {
 	return g.src(html.srcFiles).pipe(g.dest(html.converted));
 });
 
-g.task("convert:html", () => {
+g.task('convert:html', () => {
 	return (
 		g
 			.src(html.srcFiles)
@@ -428,7 +429,7 @@ g.task("convert:html", () => {
  * @task  {clean:html}
  * @group {Utilities}
  */
-g.task("clean:html", () => {
+g.task('clean:html', () => {
 	return del.sync(html.converted);
 });
 
@@ -440,12 +441,12 @@ g.task("clean:html", () => {
  * @task  {concat}
  * @group {Production}
  */
-g.task("concat", () => {
+g.task('concat', () => {
 	return g
 		.src(html.productionFiles)
 		.pipe(useref())
-		.pipe(gulpif("*.js", uglify()))
-		.pipe(gulpif("*.css", cssnano()))
+		.pipe(gulpif('*.js', uglify()))
+		.pipe(gulpif('*.css', cssnano()))
 		.pipe(g.dest(base.build));
 });
 
@@ -454,11 +455,11 @@ g.task("concat", () => {
  * @task  {minify}
  * @group {Production}
  */
-g.task("minify", () => {
+g.task('minify', () => {
 	return g
 		.src(html.productionFiles)
 		.pipe(htmlmin(html.opts))
-		.pipe(g.dest(base.build + "/"));
+		.pipe(g.dest(base.build + '/'));
 });
 
 /**
@@ -466,17 +467,17 @@ g.task("minify", () => {
  * @task  {tree}
  * @group {Utilities}
  */
-g.task("tree", () => {
+g.task('tree', () => {
 	var once = true; // lalz0r
-	g.src(base.build + "/**")
+	g.src(base.build + '/**')
 		.pipe(
-			map(function (file) {
+			map(function(file) {
 				if (file.path.match(base.build)) return file;
 			})
 		)
 		.pipe(filetree({ cwdRelative: true }))
 		.pipe(
-			map(function (file) {
+			map(function(file) {
 				if (once) {
 					console.log(archy(file.tree));
 					once = !once;
@@ -494,7 +495,7 @@ g.task("tree", () => {
  * @task  {fonts}
  * @group {Main}
  */
-g.task("fonts", () => {
+g.task('fonts', () => {
 	return g.src(fonts.input).pipe(g.dest(fonts.output));
 });
 
@@ -504,7 +505,7 @@ g.task("fonts", () => {
  * @task  {images}
  * @group {Main}
  */
-g.task("images", () => {
+g.task('images', () => {
 	g.src(images.input)
 		.pipe(cache(imagemin(images.opts)))
 		.pipe(g.dest(images.output));
@@ -515,7 +516,7 @@ g.task("images", () => {
  * @task  {clean}
  * @group {Utilities}
  */
-g.task("clean", function (callback) {
+g.task('clean', function(callback) {
 	// Clear out image cache
 	cache.clearAll();
 
@@ -532,23 +533,23 @@ g.task("clean", function (callback) {
  * @task  {serve}
  * @group {Main}
  */
-g.task("serve", () => {
+g.task('serve', () => {
 	browserSync.init(config.browserSync);
 	// Watch
 	if (config.watch.styles) {
-		g.watch(config.watch.styles, ["styles"]);
+		g.watch(config.watch.styles, ['styles']);
 	}
 	if (config.watch.scripts) {
-		g.watch(config.watch.scripts, ["scripts"]);
+		g.watch(config.watch.scripts, ['scripts']);
 	}
 	if (config.watch.views) {
-		g.watch(config.watch.views, ["pug"]);
+		g.watch(config.watch.views, ['pug']);
 	}
 	if (config.watch.data) {
-		g.watch(config.watch.data, ["pug"]);
+		g.watch(config.watch.data, ['pug']);
 	}
 	if (config.watch.reload) {
-		g.watch(config.watch.reload).on("change", browserSync.reload);
+		g.watch(config.watch.reload).on('change', browserSync.reload);
 	}
 });
 
@@ -557,7 +558,7 @@ g.task("serve", () => {
  * @task {help}
  * @group {Utilities}
  */
-g.task("help", () => {
+g.task('help', () => {
 	return usage(g);
 });
 
@@ -569,7 +570,7 @@ g.task("help", () => {
  * @task  {font:dependency}
  * @group {Utilities}
  */
-g.task("font:dependency", () => {
+g.task('font:dependency', () => {
 	return g.src(fonts.dependency.input).pipe(g.dest(fonts.dependency.output));
 });
 
@@ -578,7 +579,7 @@ g.task("font:dependency", () => {
  * @task  {font:dependency}
  * @group {Utilities}
  */
-g.task("owl", () => {
-	g.src(owlCarousel.inputCss).pipe(g.dest(owlCarousel.outputCss))
-	g.src(owlCarousel.inputJs).pipe(g.dest(owlCarousel.outputJs))
+g.task('owl', () => {
+	g.src(owlCarousel.inputCss).pipe(g.dest(owlCarousel.outputCss));
+	g.src(owlCarousel.inputJs).pipe(g.dest(owlCarousel.outputJs));
 });
