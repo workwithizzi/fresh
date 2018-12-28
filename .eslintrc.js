617cfd67ae471a640581b5bcd4aa493524cacbcd
IvaTsu <ivan.tsud@gmail.com>
2018-12-28 14:59:17 +0200

    Integrate ESlint with Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,8 @@
 module.exports = {
 	parser: 'babel-eslint',
 	extends: ['prettier'],
+	plugins: ['prettier'],
 	rules: {
-		quotes: ['error', 'single'],
-		indent: ['error', 'tab']
+		'prettier/prettier': ['error']
 	}
 };
